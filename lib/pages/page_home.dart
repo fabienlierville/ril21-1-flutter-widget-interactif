23e2c0ec1e8df9011511ba802b9ffe841109e494
Fabien LIERVILLE <fabien.lierville27@gmail.com>
2022-01-27 11:27:59 +0100

    fisrt

---
+++ lib/pages/page_home.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class PageHome extends StatefulWidget {
+  const PageHome({Key? key}) : super(key: key);
+
+  @override
+  _PageHomeState createState() => _PageHomeState();
+}
+
+class _PageHomeState extends State<PageHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Widgets Interactifs"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+
+          ],
+        ),
+      )
+    );
+  }
+}
